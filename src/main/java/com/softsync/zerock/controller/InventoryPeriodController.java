06e95c188b92db4260eed82cd0c33f95da9390d9
riya <9048jj@naver.com>
2024-06-04 17:17:35 +0900

    part3 fix

--- src/main/java/com/softsync/zerock/controller/InventoryPeriodController.java
+++ src/main/java/com/softsync/zerock/controller/InventoryPeriodController.java
@@ -28,11 +28,11 @@ public class InventoryPeriodController {
 			@RequestParam(value = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
 			@RequestParam(value = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
 		if (startDate == null) {
-			startDate = LocalDate.now().minusMonths(1); // 기본값: 1개월 전
-		}
-		if (endDate == null) {
-			endDate = LocalDate.now(); // 기본값: 오늘
-		}
+            startDate = LocalDate.now().withMonth(1).withDayOfMonth(1); // 기본값: 당해년도 1월 1일
+        }
+        if (endDate == null) {
+            endDate = LocalDate.now(); // 기본값: 오늘
+        }
 		List<InventoryPeriodSummaryDTO> inventoryPeriods = inventoryPeriodService.getInventoryPeriodSummaries(startDate,
 				endDate);
 		model.addAttribute("inventoryPeriods", inventoryPeriods);
