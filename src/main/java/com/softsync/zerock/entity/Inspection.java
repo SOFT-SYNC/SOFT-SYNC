946e9f482c1a635b585dd2405caf3dd8ab31b034
riya <9048jj@naver.com>
2024-06-07 10:01:38 +0900

    브랜치 안땀..

--- src/main/java/com/softsync/zerock/entity/Inspection.java
+++ src/main/java/com/softsync/zerock/entity/Inspection.java
@@ -1,7 +1,6 @@
 package com.softsync.zerock.entity;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
